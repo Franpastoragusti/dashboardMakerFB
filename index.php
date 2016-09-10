3d9f645b4f4ef7747f8d42c349b20265a644089c
Franpastoragusti <franciscopa91@gmail.com>
2016-09-11 01:04:25 +0200

    prueba

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <html>
     <head>
-        <script type="text/javascript" src="https//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+    <meta charset="utf-8">
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
-        <script type="text/javascript" src="excel-builder/dist/excel-builder.dist.js"></script>
+        <script type="text/javascript" src="./excel-builder/dist/excel-builder.dist.js"></script>
        
         <script type="text/javascript" src="app/js/functions.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/css/uikit.min.css"/>
@@ -13,7 +14,7 @@
         <section>
         <div class="uk-text-center">
             <div class="uk-img-div uk-align-center">
-                <img src="img/Facebook-create.png">
+                <img src="app/img/Facebook-create.png">
             </div>
             <div class="uk-vertical-align-middle">
            
@@ -21,12 +22,12 @@
                  require_once "app/functions/functions.php";
                  require_once "app/functions/pageGenerator.php";
                     if (empty($_POST)) {
-                        $page=load_page('./views/form.php');
+                        $page=load_page('app/views/form.php');
                         view_page($page);
 
                     }else{
                         $json=parseCSVFacebookByDays($_POST["csv"]);
-                        $page=load_page('./views/form2.php');
+                        $page=load_page('app/views/form2.php');
                         $page=$page."<div class='uk-form-row'><a class='uk-width-1-1 uk-button uk-button-link uk-button-large' href='#' id='downloader' download=".$_POST['title'].".xlsx>Descarga</a></div></form>";
                         view_page($page);
                     }
