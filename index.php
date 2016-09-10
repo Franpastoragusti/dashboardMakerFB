ed48c9ba51e700a3605d1307ba1e8abb4a5f196a
Franpastoragusti <franciscopa91@gmail.com>
2016-09-11 00:51:21 +0200

    preparado aws

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <html>
     <head>
-        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+        <script type="text/javascript" src="https//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
-        <script type="text/javascript" src="../excel-builder/dist/excel-builder.dist.js"></script>
+        <script type="text/javascript" src="excel-builder/dist/excel-builder.dist.js"></script>
        
-        <script type="text/javascript" src="js/functions.js"></script>
+        <script type="text/javascript" src="app/js/functions.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/css/uikit.min.css"/>
-        <link rel="stylesheet" href="css/style.css"/>
+        <link rel="stylesheet" href="app/css/style.css"/>
     </head>
     <body>
         <section>
@@ -18,8 +18,8 @@
             <div class="uk-vertical-align-middle">
            
                 <?php 
-                 require_once "functions/functions.php";
-                 require_once "functions/pageGenerator.php";
+                 require_once "app/functions/functions.php";
+                 require_once "app/functions/pageGenerator.php";
                     if (empty($_POST)) {
                         $page=load_page('./views/form.php');
                         view_page($page);
